f53c7bd7c6ff431a03d94d617946e084f241d638
Noah Herrin <noahherrin@gmail.com>
2020-11-12 11:54:46 -0800

    implemented basic responsive client server communication

--- client.py
+++ client.py
@@ -1,13 +1,40 @@
 import socket
 import json
+import argparse
+import sys
 
-HOST = '127.0.0.1'
-PORT = 5003
-s = socket.socket()
-s.connect((HOST, PORT))
-try:
-    color = raw_input('enter a color [red/green/blue]: ')
-    s.send(color.encode())
-except KeyboardInterrupt:
-    s.close()
-    print('bye')
+if __name__ == '__main__':
+
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-host', help='-host <server address>')
+    parser.add_argument('-port', help='-port <server port>')
+    args = parser.parse_args()
+
+    address = '127.0.0.1' # default address
+    port = 5003 # default port
+    if args.host:
+        address = args.host
+    if args.port:
+        port = int(args.port)
+    
+    server = socket.socket()
+    server.connect((address, port))
+    while True:
+        msg = raw_input("> ")
+        server.send(msg)
+        data = server.recv(1024)
+        if data != None:
+            msg = data.decode('ascii')
+            print(msg)
+        else:
+            break
+         
+    server.close()
+
+
+
+    
+    
+    server_address = None
+    server_port = None
--- server.py
+++ server.py
@@ -15,10 +15,12 @@ import time
 import math
 import socket
 import threading
+import random
 from rpi_ws281x import PixelStrip, Color
 
 # Animation Constants
 ACTIVE= Color(0,0,0)
+NEW_REQUEST = False
 ENABLED = True
 LED_COUNT = 300        # Number of LED pixels.
 LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
@@ -35,7 +37,6 @@ PORT = 5003
 QUEUE_LEN = 5
 
 def server():
-    global ACTIVE
     # initialize socket, and set it to listen.
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind(("localhost", PORT))
@@ -43,53 +44,26 @@ def server():
     
     while True:
         print('Waiting connections.')
-        client_socket, address = server.accept()
-        data = client_socket.recv(1024)
-        if data != None:
-            msg = data.decode("ascii").lower()
-            if msg == 'red':
-                ACTIVE = Color(255,0,0)
-            elif msg == 'blue':
-                ACTIVE = Color(0,0,255)
-            elif msg == 'green':
-                ACTIVE = Color(0,255,0)
-            else:
-                print("unknown color.")
-        client_socket.close()
-        
-def setup():
-    strip = PixelStrip(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-    strip.begin()
-    return strip
-
-def colorWipe(strip, color, wait_ms=50):
-    """Wipe color across display a pixel at a time."""
-    for i in range(strip.numPixels()):
-        strip.setPixelColor(i, color)
-        strip.show()
-        time.sleep(wait_ms / 1000.0)
-
-def update(strip, lights, color):
-    for idx in range(0, len(lights) -1):
-        strip.setPixelColor(idx, lights[idx])
-    lights.pop(0)
-    lights.append(color)
-
-def led_engine():
-    global ACTIVE
-    strip = setup()
-    leds = [Color(0,0,0) for i in range(LED_COUNT)]
-    update(strip, leds, Color(0,0,0))
-    strip.show()
-    while ENABLED:
-        print("hi")
-        update(strip, leds, ACTIVE)
-        strip.show()
-    print('done')
-
+        client, address = server.accept()
+        msg = ''
+        while msg != 'end':
+            data = client.recv(1024)
+            if data != None:
+                msg = data.decode('ascii').lower()
+                if msg == 'color':
+                    client.send(random.choice(['red', 'blue', 'green']))
+                elif msg == 'number':
+                    client.send(str(random.randint(1,10)))
+                else:
+                    client.send('I do not understand.')
+        client.close()
+def update_animation():
+    change_animation
+    call_animation(args)
+def animation1():
+    while not NEW_REQUEST:
+        # animate
+    
 if __name__ == '__main__':
-    print("Hello, World!")
-    engine = threading.Thread(target=server)
-    engine.start()
-    # engine.join()
-    led_engine()
+    server()
+
